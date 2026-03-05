4ed99e884aa1ff8371f0761360640c522478e5f3
raadplmnko-web <raad.plmnko@gmail.com>
2026-03-05 09:50:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,17 +9,18 @@ BOT_TOKEN = os.getenv("TOKEN")
 MY_CHAT_ID = int(os.getenv("CHAT_ID"))
 
 async def main():
-    # إنشاء العميل
-    client = TelegramClient('session_name', API_ID, API_HASH)
+    # استخدام اسم جلسة جديد لتجنب القفل
+    client = TelegramClient('session_new_radar', API_ID, API_HASH)
     notification_bot = Bot(token=BOT_TOKEN)
     
-    # محاولة الدخول برقمك والكود الذي وصلك تلقائياً
-    # استبدل XXXXX بالرقم الذي سيصلك الآن
-    await client.start(phone='+966548768843', code_callback=lambda: '23385') 
+    print("📡 الرادار يحاول الاتصال... راقب تطبيق تليجرام الآن")
     
-    print("🚀 نجح الاتصال! الرادار يعمل الآن.")
+    # سيطلب الكود، وبما أننا على الجوال، سنحاول إدخاله مرة أخيرة يدوياً
+    await client.start(phone='+966548768843')
+    
+    print("🚀 تم الاتصال بنجاح!")
 
-    @client.on(events.NewMessage(chats='@اسم_قناتك'))
+    @client.on(events.NewMessage(chats='@اسم_القناة'))
     async def handler(event):
         if '$' in event.raw_text:
             await notification_bot.send_message(chat_id=MY_CHAT_ID, text=f"🎯 صيد:\n{event.raw_text}")
