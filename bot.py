43cac89ef67056b73ce6d609115805cc79894239
raadplmnko-web <raad.plmnko@gmail.com>
2026-03-06 02:27:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,18 @@
 import os
 import asyncio
-from telethon import TelegramClient, events
-from telegram import Bot
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
+# جلب البيانات من Render
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("TOKEN")
-MY_CHAT_ID = int(os.getenv("CHAT_ID"))
 
-async def get_code():
-    print("⏳ بانتظار كتابة الكود في ملف code.txt في GitHub...")
-    while True:
-        if os.path.exists("code.txt"):
-            with open("code.txt", "r") as f:
-                code = f.read().strip()
-                if code and len(code) >= 5:
-                    return code
-        await asyncio.sleep(3)
-
-async def main():
-    # استخدام اسم جلسة جديد لضمان الاتصال النظيف
-    client = TelegramClient('session_radar_final', API_ID, API_HASH)
-    notification_bot = Bot(token=BOT_TOKEN)
-    
-    print("📡 الرادار يحاول الاتصال... راقب تليجرام الآن")
-    
-    # سيطلب الكود وينتظر كتابته في code.txt
-    await client.start(phone='+966548768843', code_callback=get_code)
-    print("🚀 تم الاتصال بنجاح! الرادار يراقب القناة الآن.")
-
-    # تم وضع رابط قناتك الخاصة هنا
-    @client.on(events.NewMessage(chats='https://t.me/+QfEEuAbj1wA5MGU8'))
-    async def handler(event):
-        if event.raw_text and '$' in event.raw_text:
-            await notification_bot.send_message(chat_id=MY_CHAT_ID, text=f"🎯 صيد جديد من الرادار:\n\n{event.raw_text}")
-
-    await client.run_until_disconnected()
+async def get_session():
+    # إنشاء جلسة مؤقتة لاستخراج المفتاح
+    async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+        print("🔑 المـفـتـاح الـذهـبـي (Session String) هو:")
+        print(client.session.save())
+        print("👆 انسخ النص الطويل أعلاه وضعه في Render")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(get_session())
